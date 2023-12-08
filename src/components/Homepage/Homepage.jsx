dd74b737ca6c0001e5016a3e6f89264a89b3bb7c
Bruno Fina <bruno-fina@hotmail.fr>
2023-12-08 14:53:30 +0400

    Homepage and Navbar update

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,8 +1,12 @@
-import Navbar from "../Navbar/Navbar";
+import "../Homepage/homepage.css"
+import Navbar from "../Navbar/Navbar.jsx";
+import Landpage from "../Landpage/Landpage.jsx";
+
 
 export default function Homepage() {
 
     return <>
         <Navbar />
+        <Landpage />
     </>
 }
